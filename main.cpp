cb459f259746050121a7ef0f02f21c9a9571b43c
StepManOfficial <82768983+stepmanofficial@users.noreply.github.com>
2021-11-08 21:53:00 +0300

    Add idle func; Comment changes; Func rename

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ void WindowDisplay() {
       glVertex2f(0.1f, -0.6f);
       glVertex2f(0.7f, -0.6f);
       glVertex2f(0.4f, -0.1f);
- 
+
       glColor3f(1.0f, 0.0f, 0.0f); // Red
       glVertex2f(0.3f, -0.4f);
       glColor3f(0.0f, 1.0f, 0.0f); // Green
@@ -52,37 +52,33 @@ void WindowDisplay() {
 
 // Настройка матрицы проекции перед отображением окна (Оптимизация под работу с 2D)
 void WindowResize(GLsizei  w, GLsizei  h) { // GLsizei - non-negative integer
-    if (h == 0) h = 1; // To prevent divide by 0
+    if (h == 0)                  // To prevent divide by 0
+        h = 1;
     GLfloat aspectRatio = (GLfloat)w / (GLfloat)h;
-    glViewport(0, 0, w, h); // Set the viewport to cover the window
+    glViewport(0, 0, w, h);      // Set the viewport to cover the window
 
     glMatrixMode(GL_PROJECTION); // To operate on the Projection matrix
     glLoadIdentity();            // Reset the projection matrix
 
     if (w >= h) {
-        // aspect >= 1, with larger width
-        // gluOrtho2D(-1.0 * aspectRatio, 1.0 * aspectRatio, -1.0, 1.0);
+        // If larger width
         glOrtho(-1.0 * aspectRatio, 1.0 * aspectRatio, -1.0, 1.0, 0.0, 1.0);
     } else {
-        // gluOrtho2D(-1.0, 1.0, -1.0 / aspectRatio, 1.0 / aspectRatio);
+        // If larger height
         glOrtho(-1.0, 1.0, -1.0 / aspectRatio, 1.0 / aspectRatio, 0.0, 1.0);
     }
-/*     X: from 0 to windowWidth
-       Y: from 0 to windowHeight FLIPPED
-       Z: from 0 to 1 (geometry not rendered outside)
-       Координаты начинаются с ЛЕВОГО ВЕРХНЕГО угла */
-    // glOrtho(-1.0, 1.0, -1.0, 1.0, 0.0, 1.0);
-    // glMatrixMode(GL_MODELVIEW);
-    // glLoadIdentity();
 }
 
 int main(int argc, char **argv) {
     // Инициализация GLUT
     glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    // Создание окна
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Test Window");
+    // glutFullScreen();
 
     // Регистрация обратных вызовов
     glutDisplayFunc(WindowDisplay);
