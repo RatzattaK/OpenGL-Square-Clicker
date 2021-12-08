da815983348d31e17070d0da6c90232312075d78
StepManOfficial <82768983+stepmanofficial@users.noreply.github.com>
2021-12-08 22:53:32 +0300

    add first upgrade

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <GL/freeglut.h>
 #include <string>
 #include <sstream>
+#include <cmath>
 using namespace std;
 
 int windowWidth  = 600;
@@ -8,26 +9,106 @@ int windowHeight = 600;
 
 void *defaultFont = GLUT_BITMAP_HELVETICA_18;
 
-GLfloat clrCream[] = {0.99, 0.964, 0.929};
-GLfloat clrAqua[] = {0.407, 0.53, 0.53};
-GLfloat clrWhite[] = {1.0, 1.0, 1.0};
-GLfloat clrBlack[] = {0.0, 0.0, 0.0};
-GLfloat clrGreen[] = {0.713, 0.843, 0.658};
+GLfloat clrCream[3] = {0.99, 0.964, 0.929};
+GLfloat clrAqua[3] = {0.407, 0.53, 0.53};
+GLfloat clrWhite[3] = {1.0, 1.0, 1.0};
+GLfloat clrBlack[3] = {0.0, 0.0, 0.0};
+GLfloat clrGreen[3] = {0.713, 0.843, 0.658};
+
+class Clicks {
+public:
+    float amount = 0;
+    float perSec = 0;
+};
+Clicks clicks;
+
+const char *ConvertToChar(int val) {
+        stringstream sstream;
+        sstream << val;
+        string str = sstream.str();
+        return str.c_str();
+}
+
+const char *ConvertToChar(float val) {
+        stringstream sstream;
+        sstream.precision(2);
+        sstream << fixed << val;
+        string str = sstream.str();
+        return str.c_str();
+}
+
+void DisplayText(float _x, float _y, const GLfloat *v, void *font, const char *_str) {
+    glColor3fv(v);
+    glRasterPos2f(_x, _y);
+    int len, i;
+    len = (int)strlen(_str);
+    for (i = 0; i < len; i++)
+        glutBitmapCharacter(font, _str[i]);
+}
+
+class UpgradeButton {
+public:
+    int baseCost;
+    float multiplier = 1.07;
+    int owned = 0;
+    float price;
+
+    float x;
+    float y;
+    float w;
+    float h;
+    GLfloat color[3];
+
+    void Draw() {
+        glColor3fv(clrCream);
+        glBegin(GL_QUADS);
+            glVertex2f(x + w, y);
+            glVertex2f(x + w, y + h);
+            glVertex2f(x, y + h);
+            glVertex2f(x, y);
+        glColor3fv(clrGreen);
+            glVertex2f(x + w + 10, y);
+            glVertex2f(x + w + 10, y + h);
+            glVertex2f(x + w, y + h);
+            glVertex2f(x + w, y);
+        glEnd();
+        DisplayText(x + 2, y + 5, clrBlack, defaultFont, "Auto Click");
+        DisplayText(x + w - 10, y + 5, clrBlack, defaultFont, ConvertToChar((int)price));
+        DisplayText(x + w + 2, y + 5, clrCream, defaultFont, ConvertToChar(owned));
+    }
+
+    void OnClick() {
+        if (price <= clicks.amount) {
+            clicks.amount -= price;
+            clicks.perSec += 0.2;
+            owned++;
+            price = baseCost * powf(multiplier, owned);
+        }
+    }
+
+    UpgradeButton(int upgradeCost, float _x, float _y, float width, float height) {
+        baseCost = upgradeCost;
+        price = baseCost * powf(multiplier, owned);
+        x = _x;
+        y = _y;
+        w = width;
+        h = height;
+    }
+};
+UpgradeButton btnAutoClicks(10, 50, 20, 35, 8);
 
 class UI {
 public:
-    int clicks = 0;
-    float autoClicks = 0;
-    const char *numericChar;
+    // const char *numericChar;
 
-    void DisplayText(float _x, float _y, const GLfloat *v, void *font, const char *_str) {
-        glColor3fv(v);
-        glRasterPos2f(_x, _y);
-        int len, i;
-        len = (int)strlen(_str);
-        for (i = 0; i < len; i++)
-            glutBitmapCharacter(font, _str[i]);
-    }
+    // void DisplayText(float _x, float _y, const GLfloat *v, void *font, const char *_str) {
+    //     glColor3fv(v);
+    //     glRasterPos2f(_x, _y);
+    //     int len, i;
+    //     len = (int)strlen(_str);
+    //     for (i = 0; i < len; i++)
+    //         glutBitmapCharacter(font, _str[i]);
+    // }
 
     void DrawHUD() {
         // Lines
@@ -46,6 +127,7 @@ public:
         glEnd();
         glPopMatrix();
         // drawLevel();
+        btnAutoClicks.Draw();
         DisplayClickCount();
     }
 
@@ -70,19 +152,13 @@ public:
     }
 
     void DisplayClickCount() {
-        stringstream temp_str;
-        temp_str << (clicks);
-        string str = temp_str.str();
-        numericChar = str.c_str();
         DisplayText(70, 7, clrWhite, defaultFont, "CLICKS:");
-        DisplayText(90, 7, clrGreen, defaultFont, numericChar);
+        DisplayText(85, 7, clrGreen, defaultFont, ConvertToChar(clicks.amount));
     }
 
     // TODO: Меню при входе в игру (вы заработали...)
     void drawIntro() {
-        // DisplayText(10, 10, 0.0, 0.0, 1.0, GLUT_BITMAP_TIMES_ROMAN_24, "Welcome to SIMPLECLICKER!");
-        // DisplayText(10, 20, 0.0, 0.0, 1.0, GLUT_BITMAP_HELVETICA_18, "Hit targets with your mouse and progress through levels to GAIN MORE SCORE!");
-        // DisplayText(33, 50, 1.0, 0.0, 0.0, GLUT_BITMAP_TIMES_ROMAN_24, "PRESS SPACE TO START");
+        DisplayText(33, 50, clrBlack, GLUT_BITMAP_TIMES_ROMAN_24, "PRESS ENTER TO START");
         DisplayText(10, 80, clrWhite, GLUT_BITMAP_HELVETICA_12, "Made by Alexander Tatchin");
         DisplayText(10, 85, clrWhite, GLUT_BITMAP_HELVETICA_12, "Implemented via Visual C++ / Freeglut OPEN_GL");
         DisplayText(10, 90, clrWhite, GLUT_BITMAP_HELVETICA_12, "Feb 2015");
@@ -92,22 +168,25 @@ public:
         DisplayText(27, 50, clrGreen, GLUT_BITMAP_9_BY_15, "ARE YOU SURE YOU WANT TO EXIT?");
     }
 };
-
 UI ui;
 
 class ObjMouse {
 public:
+    float x = 15;
+    float y = 40;
+    float size = 20;
     void Draw() {
         glBegin(GL_QUADS);
         glColor3fv(clrGreen);
-            glVertex2f(35, 40);
-            glVertex2f(35, 60);
-            glVertex2f(15, 60);
-            glVertex2f(15, 40);
+            glVertex2f(x + size, y       );
+            glVertex2f(x + size, y + size);
+            glVertex2f(x       , y + size);
+            glVertex2f(x       , y       );
         glEnd();
     }
     void Animate() {
         glClear(GL_COLOR_BUFFER_BIT);
+
         glPushMatrix();
         ui.DrawHUD();
         glTranslatef(0.0, 0.02, 0.0);
@@ -166,11 +245,6 @@ void DisplayScene() {
     glutSwapBuffers();
 }
 
-void Anim() {
-    MouseObj.Draw();
-    // glutPostRedisplay();
-}
-
 // Настройка матрицы проекции перед отображением окна (Корректное соотношение сторон)
 void WindowResize(GLsizei  w, GLsizei  h) { // GLsizei - non-negative integer
     if (h == 0)                  // To prevent divide by 0
@@ -215,29 +289,46 @@ void ProcessKeyPress(unsigned char key, int x, int y) {
     // Spacebar
     case 32:
         if (director.gameState == 2) {
-            ui.clicks++;
+            clicks.amount++;
             glutPostRedisplay();
         }
         break;
     }
 }
 
-bool IsClickedInsideObject(double winX, double winY) {
-    double x = double(winX) / windowWidth * (100 - 0) + 0;
-    double y = double(winY) / windowHeight * (0 - 100) + 100;
-    return (x > 15 && y > 40 &&
-            x < 35 && y < 60);
+bool IsClickedInsideObject(double winX, double winY, float objX, float objY, float objW, float objH) {
+    double x = double(winX) / windowWidth * 100;
+    double y = double(winY) / windowHeight * 100;
+    return (x > objX && y > objY &&
+            x < objX + objW && y < objY + objH);
 }
 
 void ProcessMousePress(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        if (director.gameState == 1)
+        switch (director.gameState)
+        {
+        case 1:
             director.gameState = 2;
-        if ( director.gameState == 2 &&  IsClickedInsideObject(x, y)) {
-            MouseObj.Animate();
-            ui.clicks++;
+            break;
+        case 2:
+            // Главный объект
+            if (IsClickedInsideObject(x, y, MouseObj.x, MouseObj.y, MouseObj.size, MouseObj.size)) {
+                MouseObj.Animate();
+                clicks.amount++;
+            }
+            // BtnAutoClicks
+            if (IsClickedInsideObject(x, y, btnAutoClicks.x, btnAutoClicks.y, btnAutoClicks.w, btnAutoClicks.h)) {
+                btnAutoClicks.OnClick();
+            }
+            break;
         }
     }
+}
+
+void TimerFunc(int) {
+    clicks.amount += clicks.perSec;
+    glutPostRedisplay();
+    glutTimerFunc(1000, TimerFunc, 1);
 }
 
 int main(int argc, char **argv) {
@@ -254,8 +345,8 @@ int main(int argc, char **argv) {
     glOrtho(0.0, 100, 100, 0, -1.0, 1.0);
 
     // Отображение
-    // glutIdleFunc(Anim);
     glutDisplayFunc(DisplayScene);
+    TimerFunc(0);
     // Изменение размера окна
     glutReshapeFunc(WindowResize);
 
